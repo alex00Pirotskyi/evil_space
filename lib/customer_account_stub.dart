5f478909f3291f988b650a7ff4d4f7fda227f01f
WLD <alex.p@wearabledevices.co.il>
2026-09-09 21:20:40 +0300

    Add non-web customer account stub

---
+++ lib/customer_account_stub.dart
@@ -0,0 +1,30 @@
+import 'customer_account_models.dart';
+
+class CustomerAccountApi {
+  Future<CustomerAccountSnapshot> snapshot() {
+    throw const CustomerAccountException('Customer accounts are available on web.');
+  }
+
+  Future<PhoneChallenge> startPhone(String name, String phone) {
+    throw const CustomerAccountException('Phone registration is available on web.');
+  }
+
+  Future<CustomerAccountSnapshot> verifyPhone(
+    String challenge,
+    String code,
+  ) {
+    throw const CustomerAccountException('Phone registration is available on web.');
+  }
+
+  Future<TelegramSignup> startTelegram() {
+    throw const CustomerAccountException('Telegram registration is available on web.');
+  }
+
+  Future<CustomerAccountSnapshot?> telegramStatus(String token) async => null;
+
+  Future<CustomerAccountSnapshot> signInGoogle(String idToken) {
+    throw const CustomerAccountException('Google registration is available on web.');
+  }
+
+  Future<void> logout() async {}
+}
