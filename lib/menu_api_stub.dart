00e1912b206ee49b5b27e05abce69b52e61f2bff
WLD <alex.p@wearabledevices.co.il>
2026-09-09 05:19:18 +0300

    Add non-web menu API stub

---
+++ lib/menu_api_stub.dart
@@ -0,0 +1,27 @@
+import 'menu_models.dart';
+
+class MenuApi {
+  Future<MenuCatalog> menu() async => const MenuCatalog.empty();
+
+  Future<MenuOrderPayment> createOrder(String itemId) {
+    throw const MenuApiException('Menu ordering is available on web.');
+  }
+
+  Future<MenuOrderStatus> orderStatus(String token) {
+    throw const MenuApiException('Menu ordering is available on web.');
+  }
+
+  Future<AdminMenuSnapshot> adminSnapshot() {
+    throw const MenuApiException('Menu admin is available on web.');
+  }
+
+  Future<AdminMenuSnapshot> uploadMenuJson(Map<String, dynamic> menu) {
+    throw const MenuApiException('Menu admin is available on web.');
+  }
+
+  Future<Map<String, dynamic>?> pickMenuJson() async => null;
+
+  Future<AdminMenuSnapshot> markPaid(int id) {
+    throw const MenuApiException('Menu admin is available on web.');
+  }
+}
