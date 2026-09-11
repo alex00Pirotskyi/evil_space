13ceb2c692b8a5926f4b8767fe9bbe5cbffd55bb
WLD <alex.p@wearabledevices.co.il>
2026-09-11 21:28:17 +0300

    Mirror commerce API in non-web stub

--- lib/menu_api_stub.dart
+++ lib/menu_api_stub.dart
@@ -7,9 +7,16 @@ class MenuApi {
     throw const MenuApiException('Menu ordering is available on web.');
   }
 
-  Future<MenuOrderPayment> createCartOrder(Map<String, int> cart) {
+  Future<MenuOrderPayment> createCartOrder(
+    Map<String, int> cart, {
+    int? promoGrantId,
+  }) {
     throw const MenuApiException('Menu ordering is available on web.');
   }
+
+  Future<List<PromoPreview>> eligiblePromos(Map<String, int> cart) async => const [];
+  Future<List<CustomerPromo>> customerPromos() async => const [];
+  Future<List<CustomerPromo>> claimPromo(String code) async => const [];
 
   Future<MenuOrderStatus> orderStatus(String token) {
     throw const MenuApiException('Menu ordering is available on web.');
@@ -21,6 +28,40 @@ class MenuApi {
 
   Future<AdminMenuSnapshot> uploadMenuJson(Map<String, dynamic> menu) {
     throw const MenuApiException('Menu admin is available on web.');
+  }
+
+  Future<MenuDraftSnapshot> menuDraft() {
+    throw const MenuApiException('Menu admin is available on web.');
+  }
+
+  Future<MenuDraftSnapshot> saveMenuDraft(MenuCatalog menu) {
+    throw const MenuApiException('Menu admin is available on web.');
+  }
+
+  Future<(AdminMenuSnapshot, MenuDraftSnapshot)> publishMenuDraft(MenuCatalog menu) {
+    throw const MenuApiException('Menu admin is available on web.');
+  }
+
+  Future<List<AdminPromotion>> adminPromotions() async => const [];
+  Future<List<AdminPromotion>> createPromotion(Map<String, dynamic> promo) async => const [];
+  Future<List<AdminPromotion>> updatePromotion(Map<String, dynamic> promo) async => const [];
+  Future<List<AdminPromotion>> disablePromotion(int id) async => const [];
+  Future<List<AdminCustomerSummary>> adminCustomers({String query = ''}) async => const [];
+
+  Future<AdminCustomerDetail> adminCustomer(int id) {
+    throw const MenuApiException('Customer admin is available on web.');
+  }
+
+  Future<AdminCustomerDetail> grantCustomerPromo({
+    required int customerId,
+    required int promotionId,
+    int uses = 1,
+  }) {
+    throw const MenuApiException('Customer admin is available on web.');
+  }
+
+  Future<AdminCustomerDetail> revokeCustomerPromo(int grantId) {
+    throw const MenuApiException('Customer admin is available on web.');
   }
 
   Future<Map<String, dynamic>?> pickMenuJson() async => null;
